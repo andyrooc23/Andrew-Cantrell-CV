fbaa4854baf5fb2a24718a8616501b10b6294fa0
andrewcantrell <andyroocantrell@gmail.com>
2021-11-26 23:03:39 -0800

    A lot of changes thanksgiving break

--- whatImListeningTo/script.js
+++ whatImListeningTo/script.js
@@ -1,3 +1,22 @@
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+
+const optionsList = document.querySelectorAll(".option");
+const playlistSelected = document.querySelector(".spotifyFrame")
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+        playlistSelected.src = o.querySelector("input").getAttribute("src");
+    });
+});
+
 function myPage() {
     window.location = ("../index.html");
 }
