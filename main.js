36324417567ce355719d08505459705452eae2fc
andrewcantrell <andyroocantrell@gmail.com>
2021-11-29 20:28:16 -0800

    Moved everything

---
+++ main.js
@@ -0,0 +1,9 @@
+const box = document.querySelector("perspective-text")
+
+box.addEventListener("click", () => {
+    window.location("main/index.html");
+});
+
+function myPage() {
+    window.location = ("main/index.html");
+}
